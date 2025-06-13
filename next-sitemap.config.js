401d17d4afef4433191a62514f0f07e926578eca
Rohith Shinoj <rohith@Mac.lan>
2025-06-13 18:31:23 +0400

    clean url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://blog1-nu-two.vercel.app',
+  siteUrl: 'https://deep-thoughts-blog.vercel.app',
   generateRobotsTxt: true, // optional but recommended
 }
